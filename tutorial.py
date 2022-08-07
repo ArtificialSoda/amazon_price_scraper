cc02e89031b80a39c76907563847e51a98964505
Fabien Dimitrov <fdimitrov@coveo.com>
2022-08-06 21:07:34 -0400

    Add attachment (image) to email in tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,6 @@
 import smtplib
 import os
+import imghdr
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = os.environ.get('TEST_EMAIL')
@@ -7,10 +8,18 @@ EMAIL_PASSWORD = os.environ.get('TEST_PASSWORD')
 
 msg = EmailMessage()
 
-msg['Subject'] = 'Hi!'
+msg['Subject'] = 'Hi! Check out this new leaked GTA6 cover!'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'xynnax6@gmail.com'
-msg.set_content("How are you?")
+msg.set_content('See image attached... Crazy, right?')
+
+with open('assets/gta6-cover.jpg', 'rb') as f:
+    file_data = f.read()
+    file_name = f.name
+    file_type = imghdr.what(file_name)
+    print(file_type)
+
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
