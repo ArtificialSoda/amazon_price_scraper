ee8453cacefb81d596f767132fcee6420cbe341a
Fabien Dimitrov <fdimitrov@coveo.com>
2022-08-06 20:09:49 -0400

    Configure environment variables for email and password (Google AppDirect) instead of manually typing them in and out, inside tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,14 @@
 import smtplib
+import os
 
+EMAIL_ADDRESS = os.environ.get('TEST_EMAIL')
+EMAIL_PASSWORD = os.environ.get('TEST_PASSWORD')
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
 
