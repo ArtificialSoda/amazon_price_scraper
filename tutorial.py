8eedc4061e555d65b496ead8d8aa5fde7f9397f8
Fabien Dimitrov <fdimitrov@coveo.com>
2022-08-06 20:38:57 -0400

    Add use of EmailMessage class, to use smtp.send_message(), for increased efficiency and readability in tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,21 @@
 import smtplib
 import os
+from email.message import EmailMessage
 
 EMAIL_ADDRESS = os.environ.get('TEST_EMAIL')
 EMAIL_PASSWORD = os.environ.get('TEST_PASSWORD')
 
+msg = EmailMessage()
+
+msg['Subject'] = 'Hi!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'xynnax6@gmail.com'
+msg.set_content("How are you?")
+
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
 
-    subject = 'Hi!'
-    body = 'How are you?'
-
-    msg = f'Subject: {subject}\n\n{body}'
-
-    smtp.sendmail(EMAIL_ADDRESS, 'xynnax6@gmail.com', msg)
     print("SUCCESS!")
 
 
