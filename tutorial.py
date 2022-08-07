2d711a5917d58bc7675ecd67d8a2abe60b621380
Fabien Dimitrov <fdimitrov@coveo.com>
2022-08-06 20:30:15 -0400

    Send simple plaintext email on tutorial.py. Changed smptlib.SMPT to smptlib.SMPT_SSL to make code more efficient

--- tutorial.py
+++ tutorial.py
@@ -4,11 +4,17 @@ import os
 EMAIL_ADDRESS = os.environ.get('TEST_EMAIL')
 EMAIL_PASSWORD = os.environ.get('TEST_PASSWORD')
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
-    
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'Hi!'
+    body = 'How are you?'
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, 'xynnax6@gmail.com', msg)
+    print("SUCCESS!")
+
+
 
 
